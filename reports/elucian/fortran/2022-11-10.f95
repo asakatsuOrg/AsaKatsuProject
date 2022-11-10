1cbb38c64e0b09d66f9f3644c5d8eb6ef9744763
Elucian Moise <elucian.moise@gmail.com>
2022-11-09 22:43:03 -0600

    New report: Day 80

---
+++ reports/elucian/fortran/2022-11-10.f95
@@ -0,0 +1,16 @@
+# Today's AsaKatsu
+
+**Done**
+
+* Day 80/100 #100DaysOfCode challenge;
+* Learning modern Fortran;
+* Create 10 articles about Fortran syntax;
+
+**ToDo**
+
+* Create a new language, inspired by Fortran
+
+----
+
+* [Fortran Tutorial](https://sagecode.net/fortran/index.html)
+* [Fortran Project](https://github.com/elucian/fortran-demo)
