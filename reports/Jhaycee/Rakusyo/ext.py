f1dbc95acb1c33d218785b7779fa5cb16b8c0d7a
jhaycee11 <jhayceecajiles@gmail.com>
2022-11-21 18:27:08 +0900

    add file

--- reports/Jhaycee/Rakusyo/ext.py
+++ reports/Jhaycee/Rakusyo/ext.py
@@ -1,23 +1,7 @@
-import re
-"""def validate(inp):
+def validate(inp):
     if inp:
         try:
             float(inp)
         except ValueError:
             return False
     return True
-"""
-"""def validate(inp):
-    if re.search(r"\d", inp) or inp == "":
-        return True
-    else:
-        return False
-"""
-
-def validate(inp):
-    if re.search(r"[0-9]", inp) or inp == "":
-        print("True")
-        return True
-    else:
-        print("False")
-        return False
